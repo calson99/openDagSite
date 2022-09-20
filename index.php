99807d9f886e3bbdfde57fcc658aa1099248b2f5
calthan leander <calsonleander99@gmail.com>
2022-09-20 11:39:42 +0200

    added basic layout

--- index.php
+++ index.php
@@ -22,4 +22,59 @@
         </nav>
         <img id="schoolImage" src="img/school.jpg" alt="school" width="100%">
     </header>
+
+    <main>
+        <div class="wrapper">
+            <div class="dualGroup">
+                <div class="borderGroupHalf">
+                    <h1>rooster</h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Nihil sequi animi minima autem sed accusamus delectus err
+                    or asperiores facere, voluptate repellendus dolor. Laboriosam
+                    , dolorem. Soluta quaerat expedita sint necessitatibus! Doloribus?</p>
+                </div>
+                <img src="img/afbeelding1.png" alt="rooster">
+            </div>
+
+            <div class="borderGroup" id="accelerate">
+                <h1>versnellen</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Nihil sequi animi minima autem sed accusamus delectus err
+                or asperiores facere, voluptate repellendus dolor. Laboriosam
+                , dolorem. Soluta quaerat expedita sint necessitatibus! Doloribus?</p>
+            </div>
+
+            <div class="borderGroup" id="vacation">
+                <h1>vakantie</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Nihil sequi animi minima autem sed accusamus delectus err
+                or asperiores facere, voluptate repellendus dolor. Laboriosam
+                , dolorem. Soluta quaerat expedita sint necessitatibus! Doloribus?</p>
+            </div>
+
+            <div class="borderGroup" id="homework">
+                <h1>huiswerk</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Nihil sequi animi minima autem sed accusamus delectus err
+                or asperiores facere, voluptate repellendus dolor. Laboriosam
+                , dolorem. Soluta quaerat expedita sint necessitatibus! Doloribus?</p>
+            </div>
+
+            <div class="borderGroup" id="groups">
+                <h1>groepen</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Nihil sequi animi minima autem sed accusamus delectus err
+                or asperiores facere, voluptate repellendus dolor. Laboriosam
+                , dolorem. Soluta quaerat expedita sint necessitatibus! Doloribus?</p>
+            </div>
+
+            <div class="borderGroup" id="vakken">
+                <h1>vakken</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Nihil sequi animi minima autem sed accusamus delectus err
+                or asperiores facere, voluptate repellendus dolor. Laboriosam
+                , dolorem. Soluta quaerat expedita sint necessitatibus! Doloribus?</p>
+            </div>
+        </div>
+    </main>
 </body>
