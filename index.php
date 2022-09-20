e7fc0c5484f6387e000a1aca425fdc5c80b9cad3
calthan leander <calsonleander99@gmail.com>
2022-09-20 11:38:11 +0200

    added header

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title></title>
+    <?php require_once 'head.php'; ?>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <img src="img/logo.png" alt="logo" class="logo" width="125px">
+            <div class="links">
+                <a href="#schedule">Rooster |</a>
+                <a href="#accelerate">Versnellen |</a>
+                <a href="#vacation">Vakantie |</a>
+                <a href="#homework">Huiswerk |</a>
+                <a href="#groups">Groepen |</a>
+                <a href="#vakken">Vakken |</a>
+                <a href="#faceBook">Smoelenboek</a>
+            </div>
+        </nav>
+        <img id="schoolImage" src="img/school.jpg" alt="school" width="100%">
+    </header>
+</body>
