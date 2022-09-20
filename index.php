c1829ad63812b33e1b712f9d5843ffc0e52b05dc
calthan leander <calsonleander99@gmail.com>
2022-09-20 11:40:34 +0200

    added footer

--- index.php
+++ index.php
@@ -77,4 +77,71 @@
             </div>
         </div>
     </main>
+
+    <footer>
+            <div class="footerSmoelenBoek">
+                <div class="smoelenBoek">
+                    <div class="person">
+                        <img src="img/download.jpeg" alt="test">
+                        <h2>steven van roosendaal</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Nihil
+                        </p>
+                    </div>
+                    <div class="person">
+                        <img src="img/wicher.jpeg" alt="test">
+                        <h2>wicher hulzebosch</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Nihil
+                        </p>
+                    </div>
+                    <div class="person">
+                        <img src="img/ine.jpeg" alt="test">
+                        <h2>ine vermeer</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Nihil
+                        </p>
+                    </div>
+                    <div class="person">
+                        <img src="img/fedje.jpeg" alt="test">
+                        <h2>fedde van gils</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Nihil
+                        </p>
+                    </div>
+                    <div class="person">
+                        <img src="img/Wiro.jpg" alt="test">
+                        <h2>wiro verdiesen</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Nihil
+                        </p>
+                    </div>
+                    <div class="person">
+                        <img src="img/mustafa.png" alt="test">
+                        <h2>mustafa kasar</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Nihil
+                        </p>
+                    </div>
+                    <div class="person">
+                        <img src="img/tim.png" alt="test">
+                        <h2>tim lutt</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Nihil
+                        </p>
+                    </div>
+                    <div class="person">
+                        <img src="img/corne.jpeg" alt="test">
+                        <h2>corne wagtmans</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Nihil
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="footer">
+                
+                <p>© calthan leander</p>
+            </div>
+    </footer>
 </body>
