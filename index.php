3ddadcf12bbe9051b3d23353b07a4680ac81451a
calthan leander <calsonleander99@gmail.com>
2022-09-30 11:34:22 +0200

    fixed footer

--- index.php
+++ index.php
@@ -100,71 +100,71 @@
     
 
     <footer>
-            <div class="footerSmoelenBoek">
-                <span class="anchor" id="faceBook"></span>
-                <div class="smoelenBoek">
-                    <div class="person">
-                        <img src="img/download.jpeg" alt="test">
-                        <h2>steven van roosendaal</h2>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Nihil
-                        </p>
-                    </div>
-                    <div class="person">
-                        <img src="img/wicher.jpeg" alt="test">
-                        <h2>wicher hulzebosch</h2>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Nihil
-                        </p>
-                    </div>
-                    <div class="person">
-                        <img src="img/ine.jpeg" alt="test">
-                        <h2>ine vermeer</h2>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Nihil
-                        </p>
-                    </div>
-                    <div class="person">
-                        <img src="img/fedje.jpeg" alt="test">
-                        <h2>fedde van gils</h2>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Nihil
-                        </p>
-                    </div>
-                    <div class="person">
-                        <img src="img/Wiro.jpg" alt="test">
-                        <h2>wiro verdiesen</h2>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Nihil
-                        </p>
-                    </div>
-                    <div class="person">
-                        <img src="img/mustafa.png" alt="test">
-                        <h2>mustafa kasar</h2>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Nihil
-                        </p>
-                    </div>
-                    <div class="person">
-                        <img src="img/tim.png" alt="test">
-                        <h2>tim lutt</h2>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Nihil
-                        </p>
-                    </div>
-                    <div class="person">
-                        <img src="img/corne.jpeg" alt="test">
-                        <h2>corne wagtmans</h2>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Nihil
-                        </p>
-                    </div>
+        <div class="footerSmoelenBoek">
+            <span class="anchor" id="faceBook"></span>
+            <div class="smoelenBoek">
+                <div class="person">
+                    <img src="img/download.jpeg" alt="test">
+                    <h2>steven van roosendaal</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Nihil
+                    </p>
+                </div>
+                <div class="person">
+                    <img src="img/wicher.jpeg" alt="test">
+                    <h2>wicher hulzebosch</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Nihil
+                    </p>
+                </div>
+                <div class="person">
+                    <img src="img/ine.jpeg" alt="test">
+                    <h2>ine vermeer</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Nihil
+                    </p>
+                </div>
+                <div class="person">
+                    <img src="img/fedje.jpeg" alt="test">
+                    <h2>fedde van gils</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Nihil
+                    </p>
+                </div>
+                <div class="person">
+                    <img src="img/Wiro.jpg" alt="test">
+                    <h2>wiro verdiesen</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Nihil
+                    </p>
+                </div>
+                <div class="person">
+                    <img src="img/mustafa.png" alt="test">
+                    <h2>mustafa kasar</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Nihil
+                    </p>
+                </div>
+                <div class="person">
+                    <img src="img/tim.png" alt="test">
+                    <h2>tim lutt</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Nihil
+                    </p>
+                </div>
+                <div class="person">
+                    <img src="img/corne.jpeg" alt="test">
+                    <h2>corne wagtmans</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Nihil
+                    </p>
                 </div>
             </div>
-            <div class="footer">
-                
-                <p>© calthan leander</p>
-            </div>
+        </div>
+        <div class="footer">
+            
+            <p>© calthan leander</p>
+        </div>
     </footer>
 </body>
 
